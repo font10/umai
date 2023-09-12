b7ac9572e7d1faef81deffcada1683cb04488802
font10 <dfont93@gmail.com>
2023-09-12 14:08:27 +0200

    update

--- src/pages/Edit_recipes/Edit.jsx
+++ src/pages/Edit_recipes/Edit.jsx
@@ -21,27 +21,7 @@ export const Edit = () => {
       .catch(error => {
         new Error(error)
       })
-  }, []);
-
-  /*if (recipe.ingredients) {
-    recipe.ingredients.map((ingredient, idx) => (
-      <div className='flex flex-row justify-center items-center py-1'>
-        <input
-          type='text'
-          key={idx}
-          name="ingredients"
-          placeholder='Enter the quantity and the ingredient(200gr rice)'
-          className='border p-1 rounded-md ps-3 text-sm h-[40px] w-full'
-          value={ingredient}
-          required
-          onChange={(evt) => handleIngredientChange(evt, idx)}
-        />
-        <button type="button" className='rounded-md text-white font-medium ml-3 w-1/12' onClick={addIngredient}><AiOutlinePlusCircle className='text-emerald-500' size={24} /></button>
-        {recipe.ingredients.length > 1 && <button type="button" className='rounded-md text-white font-medium w-1/12' onClick={delIngredient}><AiOutlineMinusCircle className='text-red-500' size={24} /></button>}
-      </div>
-    ))
-  }*/
-
+  }, [])
 
   return (
     <div className='flex flex-col'>
